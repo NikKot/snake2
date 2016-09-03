6fe5bb4bc7f764bc97a9fc15775a0e251bd24ede
NikKot <koliapuwok@mail.ru>
2016-09-03 15:26:41 +0300

    Master

--- snake2/snake2/Program.cs
+++ snake2/snake2/Program.cs
@@ -23,6 +23,11 @@ namespace snake2
             H1.Draw();
             H2.Draw();
 
+            Point p = new Point(4, 5, '*');
+
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
             Console.ReadLine();
 
         }
